01f264e6c50224d65fbbb1a6854308f4438df3e3
Keane <bkeaneneoweizhe@gmail.com>
2023-04-12 11:30:14 +0800

    working backend logic

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { messages: messages});
+});
+
+/* GET new form page. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { messages: messages});
+});
+
+/* POST new message. */
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.text, user:req.body.user, added: new Date()})
+  res.redirect('/')
+});
+
+module.exports = router;
